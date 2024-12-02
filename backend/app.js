0ee54c779c95fd924c5753b7891c014aaefad99c
Sadok <kouassisadok9@gmail.com>
2024-12-02 02:18:01 +0100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://mondial-five.vercel.app',],
+  origin: 'https://mondial-five.vercel.app',
   credentials: true
 }));
 
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,5 @@
-export const server = "https://eshop-tutorial.vercel.app/api/v2";
+export const server = "https://mondial-lp17.vercel.app/api/v2";
+
+export const backend_url = "https://mondial-lp17.vercel.app";
 
 
